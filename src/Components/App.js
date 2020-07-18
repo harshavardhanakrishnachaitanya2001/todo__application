8c1b7f7cdd83d2a6084b7b58e1add43f901088fc
Harshavardhan <187y1a0575@gmail.com>
2020-07-18 16:36:40 +0530

    COMPLETED FINALLY

--- src/Components/App.js
+++ src/Components/App.js
@@ -44,7 +44,7 @@ class App extends React.Component{
     render(){
         return (
             <div>
-                <h1 className="main-heading">TODO APPLICATION</h1>
+                <h1 className="main-heading bg-danger">TODO APPLICATION</h1>
                 <form onSubmit={this.handleSubmit}>
                     <label className="ml-5"><b>Enter the task to do:</b></label>
                     <input type="text" placeholder="Task" name="TodoTask" onChange={this.handleChange} value={this.state.todoTask}/>
@@ -52,10 +52,6 @@ class App extends React.Component{
                 </form>
                 {this.state.todoList.map(task=>{
                     return (
-                        //  <div className="tasks">
-                        //      <h2 className="mr-1">{task}</h2>
-                        //      <button type="button" onClick={this.completedTask}>DONE</button>
-                        //  </div>
                         <Task task={task} key={task.id} removeItem={this.removeItem}/>
                     )
                 })}
